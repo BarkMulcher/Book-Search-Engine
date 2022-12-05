021ddfca8e7371dd17c004f598163be62df6102a
BarkMulcher <lucas.c.haskell@gmail.com>
2022-12-05 13:38:40 -0500

    add mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,11 +11,21 @@ const typeDefs = gql`
         link: String!
         title: String!
     }
+
+    input BookInput {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
     
     type User {
         _id: ID
-        username: String!
-        email: String!
+        username: String
+        email: String
+        password: String
         savedBooks: [Book]
     }
 
